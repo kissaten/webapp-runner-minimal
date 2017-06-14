6b1ab0b911a32b47f39164dc99634bae1154ae55
Joe Kutner <jpkutner@gmail.com>
2017-06-14 10:48:52 -0500

    Added context.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
-import java.io.IOException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import java.nio.charset.Charset;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java! Charset: " + Charset.defaultCharset());
+    String var = "NONE";
+    try {
+      var = (String) new InitialContext().lookup("java:comp/env/environment");
+    } catch (NamingException e) {
+      e.printStackTrace();
+    }
+    resp.getWriter().print("environment: " + var);
   }
 }
