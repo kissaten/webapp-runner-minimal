b1e9424c16bb91531d37c8294d765b8a0384335e
BuildpackTester <jpkutner@gmail.com>
2017-10-31 19:52:33 -0700

    DrainFilter

--- src/main/java/DrainFilter.java
+++ src/main/java/DrainFilter.java
@@ -1,8 +1,37 @@
-package PACKAGE_NAME;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Joe Kutner on 10/19/17.
  *         Twitter: @codefinger
  */
-public class DrainFilter {
+public class DrainFilter implements Filter {
+
+  private AtomicInteger activeConnections = new AtomicInteger(0);
+
+  public void init(FilterConfig filterConfig) throws ServletException { }
+
+  public void doFilter(ServletRequest servletRequest,
+                       ServletResponse servletResponse,
+                       FilterChain filterChain
+                      ) throws IOException, ServletException {
+    activeConnections.incrementAndGet();
+    filterChain.doFilter(servletRequest, servletResponse);
+    activeConnections.decrementAndGet();
+  }
+
+  public void destroy() {
+    // maybe we can block on this going to zero?
+    // i think there is a semaphore thing that can have many lock holders
+    while (activeConnections.get() > 0) {
+      try { Thread.sleep(1000); } catch (InterruptedException e) { throw new RuntimeException(e);
+      }
+    }
+  }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
-import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+    System.out.println("request: " + req.getHeader("X-Request-ID"));
+//    try {
+//      Thread.sleep(25000);
+//    } catch (InterruptedException e) {
+//      e.printStackTrace();
+//    }
     resp.getWriter().print("Hello from Java! Charset: " + Charset.defaultCharset());
   }
 }
